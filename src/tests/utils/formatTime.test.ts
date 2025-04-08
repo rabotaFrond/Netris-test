f2e1e78800a726af79b0434d8f5d5541d558dd6d
rabotaFrond <rabotaFrond@yandex.ru>
2025-04-08 14:34:23 +0400

    передумал

--- src/tests/utils/formatTime.test.ts
+++
@@ -1,9 +0,0 @@
-import { formatTime } from "../../utils/formatTime";
-
-describe("formatTime", () => {
-  it("форматирует время в MM:SS:SSS", () => {
-    expect(formatTime(3012)).toBe("00:03:012");
-    expect(formatTime(65123)).toBe("01:05:123");
-    expect(formatTime(206100)).toBe("03:26:100");
-  });
-});
